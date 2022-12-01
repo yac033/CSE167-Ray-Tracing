c95c06d805031f0ccaa83df04782b6263cbb1c2b
Yahoo <yac033@ucsd.edu>
2022-11-30 17:05:45 -0800

    Update

--- include/Ray.h
+++ include/Ray.h
@@ -3,6 +3,10 @@
 #include <glm/gtx/transform.hpp>
 #include <glm/gtx/rotate_normalized_axis.hpp>
 #include <math.h>
+#include "Intersection.h"
+#include "Camera.h"
+#include "Image.h"
+#include "RTScene"
 
 
 #ifndef __RAY_H__
@@ -11,5 +15,14 @@ class Ray {
 public:
     glm::vec3 p0;
     glm::vec3 dir;
+};
+
+namespace RayTracer
+{
+    void Raytrace(Camera cam, RTScene scene, Image &image);              // page 9
+    Ray RayThruPixel(Camera cam, int i, int j, int width, int height); // page 10, 18 
+    Intersection Intersect(Ray ray, Triangle triangle);         // page 30, 33
+    Intersection Intersect(Ray ray, RTScene scene);                      // page 11, 28, 31
+    glm::vec3 FindColor(Intersection hit, int recursion_depth);        // page 15
 };
 #endif
---
+++ src/Ray.cpp
@@ -0,0 +1,1 @@
+#inlcude "Ray.h"
