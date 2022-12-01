ce7efaca479798a32cabc0254b1aa7d0b8561d07
Yahoo <yac033@ucsd.edu>
2022-11-30 17:00:54 -0800

    So far what i have

--- src/RTScene.cpp
+++ src/RTScene.cpp
@@ -4,7 +4,9 @@ Scene.cpp contains the implementation of the draw command
 #include "RTScene.h"
 #include "RTCube.h"
 #include "RTObj.h"
-
+#include "Image.h"
+#include "Ray.h"
+#include "Intersection.h"
 // The scene init definition 
 #include "RTScene.inl"
 
@@ -90,5 +92,11 @@ using namespace glm;
 //     // HW3: Your code will only be above this line.
     
 // }
-
-
+namespace RayTracer
+{
+    void Raytrace(Camera cam, RTScene scene, Image &image);              // page 9
+    Ray RayThruPixel(Camera cam, int i, int j, int width, int height); // page 10, 18 
+    Intersection Intersect(Ray ray, Triangle triangle);         // page 30, 33
+    Intersection Intersect(Ray ray, RTScene scene);                      // page 11, 28, 31
+    glm::vec3 FindColor(Intersection hit, int recursion_depth);        // page 15
+};
